1568db67c454b628341572d5d92edcf2470a4b4d
Zhanyu Wang <wang4094@gilbreth-fe01.rcac.purdue.edu>
2019-12-12 12:55:24 -0500

    add lr schedule in mnist test (pytorch)

--- mnist_test_pytorch.py
+++ mnist_test_pytorch.py
@@ -57,20 +57,31 @@ def test(args, model, device, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-        
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 3 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 3))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    return lr
+
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for testing (default: 64)')
     parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--c', type=float, default=0.1, metavar='M',
+                        help='gRDA c (default: 0.1)')
+    parser.add_argument('--mu', type=float, default=0.5, metavar='M',
+                        help='gRDA mu (default: 0.5)')
+    # parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+    #                     help='SGD momentum (default: 0.5)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -103,10 +114,12 @@ def main():
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
     model = Net().to(device)
-    optimizer = gRDA(model.parameters(), lr=0.005, c=0.1, mu=0.5)
+    optimizer = gRDA(model.parameters(), lr=args.lr, c=args.c, mu=args.mu)
     # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
+        current_lr = adjust_learning_rate(optimizer, epoch, args)
+        print("=> current learning rate:", current_lr)
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
         
